77639d72d61dbbb5cf8672680ba887a28fd6713d
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-09-23 15:08:30 -0300

    algoritmo em portugues e tutorial

---
+++ tarefa01/megasena.c
@@ -0,0 +1,37 @@
+/* ENTRADA:
+
+
+Numero de apostadores int (m) e valor da premiação double (n)
+Restrições:
+5<= m <= 1000
+1e6 <=n<=3e9
+
+SAÍDA:
+ 
+ Valor da premiação correspondente a cada apostador, sendo 1 por linha
+
+
+
+*/
+
+//ALGORTIMO EM PORTUGUES:
+
+/* Main:
+PRIMEIRA LINHA:
+recebe duas variaveis sendo a por int m e a double n
+
+SEGINDA LINHA:
+percorre o numero de vezes m as matrizes por scanf
+
+ULTIMA LINHA:
+recebe separado por espaço os 6 numeros da mega 
+
+Função para ler a matriz:
+
+Função para verificar cada matriz com os numeros:
+
+
+
+
+
+*/
