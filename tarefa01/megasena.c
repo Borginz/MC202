12b8aae6a54669a8bc1b8a0f32b9a33014dc0049
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-09-23 20:57:54 -0300

    fiz a main, ler_matriz

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -12,11 +12,11 @@ SAÍDA:
 
 
 
-*/
+
 
 //ALGORTIMO EM PORTUGUES:
 
-/* Main:
+ Main:
 PRIMEIRA LINHA:
 recebe duas variaveis sendo a por int m e a double n
 
@@ -29,26 +29,4 @@ recebe separado por espaço os 6 numeros da mega
 Função para ler a matriz:
 
 Função para verificar cada matriz com os numeros:
-
-
-
-
-
 */
-i = 0
-j = 19
-k = 2
-int contador = 0;
-for ( i = 0; i < n; i++){
-    
-    for (j = 0; j < 60; j++){
-        for (k = 0; k < 6; k++){
-            if (Matrizona[i][j] == 1 && j+1 == numeros[k]){
-                contador += 1
-            }
-        }
-    }
-    
-    Vetor_jogadores[i] = contador
-    contador = 0
-}
