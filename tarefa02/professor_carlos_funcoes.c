d847278084029416b74fb7c7688a5c6b946ef9de
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-09-29 19:13:49 -0300

    terminei a função procurar_novo

--- tarefa02/professor_carlos_funcoes.c
+++ tarefa02/professor_carlos_funcoes.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "professor_carlos.h"
 
+int comparar_nome(char novo_nome, char nome_comp){
+    int id = 0;
+    // Segue as regras da comparar_idade
+
+
+}
+
 int comparar_idade(Aluno novo, Aluno aluno_comp){
     int id = 0;
     if ( novo.nascimento.ano < aluno_comp.nascimento.ano){
@@ -25,15 +32,39 @@ int comparar_idade(Aluno novo, Aluno aluno_comp){
     }
     
 }
+
+
 Aluno procura_novo_na_turma(Turma t[], int qtd_turmas, int j){
+
     Aluno novo, aluno_comp;
     int i, id;
     novo = t[j].alunos[0];
-    for ( i = 0; i < t[j].qtd; i++){
+    for ( i = 1; i < t[j].qtd; i++){
         aluno_comp = t[j].alunos[i];
         id = comparar_idade( novo, aluno_comp);
         if ( id == -1){
-            novo = 
+            novo = t[j].alunos[i];
+
+        } else if ( id == 0){
+            id = comparar_nome(novo.nome, aluno_comp.nome);
+            if ( id == -1){
+                novo = t[j].alunos[i];
+            } else if ( id == 0){
+                id = comparar_nome(novo.sobrenome, aluno_comp.sobrenome);
+                if ( id == -1){
+                    novo = t[j].alunos[i];
+
+                }
+            }
+
+
+
 
         }
+    }
+}
+
+
+Aluno procura_velho_na_turma(Turma t[], int qtd_turmas, int j){
+    
 }
