ac9fb6c8c2a4b5def5316032ada82705540d18a5
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-09-29 20:54:52 -0300

    alterei as funções para nao repetir codigo

--- tarefa02/professor_carlos_funcoes.c
+++ tarefa02/professor_carlos_funcoes.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "professor_carlos.h"
 
-int comparar_nome(char novo_nome, char nome_comp){
+int comparar_nome(char velho_nome, char nome_comp){
     int id = 0;
     // Segue as regras da comparar_idade
 
 
 }
-
+// Retorno 1 se o primeiro parametro for mais velho que o segundo
+// -1 se mais novo 
+// 0 se igual
 int comparar_idade(Aluno novo, Aluno aluno_comp){
     int id = 0;
+
     if ( novo.nascimento.ano < aluno_comp.nascimento.ano){
         return 1;
     } else if ( novo.nascimento.ano > aluno_comp.nascimento.ano ){
@@ -25,11 +28,24 @@ int comparar_idade(Aluno novo, Aluno aluno_comp){
             } else if ( novo.nascimento.dia > aluno_comp.nascimento.dia ){
                 return -1;
             } else {
-                return 0;
+                if ( comparar_nome(novo.nome, aluno_comp.nome) == 1){
+                    return 1;
+
+
+                } else if (comparar_nome(novo.nome, aluno_comp.nome) == -1){
+                    return -1;
+                } else{
+                    if ( comparar_nome(novo.sobrenome, aluno_comp.sobrenome) == 1){
+                        return 1;
+                    } else if ( comparar_nome(novo.sobrenome, aluno_comp.sobrenome) == -1){
+                        return -1;
+                    }
+                }
             }
         }
     
-    }
+    } 
+    return 0;
     
 }
 
@@ -42,29 +58,52 @@ Aluno procura_novo_na_turma(Turma t[], int qtd_turmas, int j){
     for ( i = 1; i < t[j].qtd; i++){
         aluno_comp = t[j].alunos[i];
         id = comparar_idade( novo, aluno_comp);
-        if ( id == -1){
-            novo = t[j].alunos[i];
-
-        } else if ( id == 0){
-            id = comparar_nome(novo.nome, aluno_comp.nome);
-            if ( id == -1){
-                novo = t[j].alunos[i];
-            } else if ( id == 0){
-                id = comparar_nome(novo.sobrenome, aluno_comp.sobrenome);
-                if ( id == -1){
-                    novo = t[j].alunos[i];
-
-                }
-            }
-
-
-
-
-        }
+        if ( id == 1){
+            novo = aluno_comp;
+        } 
     }
 }
 
 
 Aluno procura_velho_na_turma(Turma t[], int qtd_turmas, int j){
-    
+    Aluno velho, aluno_comp;
+    int i, id;
+    velho = t[j].alunos[0];
+    for ( i = 1; i < t[j].qtd; i++){
+        aluno_comp = t[j].alunos[i];
+        id = comparar_idade( velho, aluno_comp);
+        if ( id == -1){
+            velho = aluno_comp;
+        }
+
+    }
+}
+
+Aluno procura_novo_todas_turmas(Turma t[], int qtd_turmas){
+    Aluno novo, aluno_comp;
+    novo = procura_novo_na_turma(t, qtd_turmas, 0);
+    for ( int j = 0; j < qtd_turmas; j++){
+        aluno_comp = procura_novo_na_turma(t, qtd_turmas, j);
+        if ( comparar_idade(novo, aluno_comp) == 1){
+            novo = aluno_comp;
+        } 
+
+    }
+    return novo;
+}
+
+  
+
+
+Aluno procura_velho_todas_turmas(Turma t[], int qtd_turmas){
+    Aluno velho, aluno_comp;
+    velho = procura_velho_na_turma(t, qtd_turmas, 0);
+    for ( int j = 0; j < qtd_turmas; j++){
+        aluno_comp = procura_velho_na_turma(t, qtd_turmas, j);
+        if ( comparar_idade(velho, aluno_comp) == -1){
+            velho = aluno_comp;
+        } 
+
+    }
+    return velho;
 }
