28672f6ccc322f978a74a4b997ead671e0d74a21
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-10-13 18:47:29 -0300

    avançando nas funçoes

--- tarefa04/seaofwords.c
+++ tarefa04/seaofwords.c
@@ -5,6 +5,32 @@ n = O numero de linhas
 m = o numero de colunas do texto
 q = numero de palavras a serem buscadas
 */
+int verificar_pos(char matriz[][MAX], int i, int j, char palavra[], int k,int n, int m){
+    if (!valido(matriz,i,j,n,m)){
+        return 0;
+    }
+    if (palavra[k] == '\0'){
+        return 1;
+    }
+    if (palavra[k] != matriz[i][j]){
+        return 0;
+    }
+    matriz[i][j] = '#';
+    verificar_pos(matriz, i+1, j, palavra, k+1,n, m);
+    verificar_pos(matriz, i-1, j, palavra, k+1,n, m);
+    verificar_pos(matriz, i, j+1, palavra, k+1,n, m);
+    verificar_pos(matriz, i, j-1, palavra, k+1,n, m);
+}
+
+
+void comparar_nome(char matriz[][MAX], char palavra[], int n, int m){
+    for ( int i = 0; i < n; i++)
+        for ( int j = 0; j < m; j++)
+       if ( matriz[i][j] == palavra[0]){
+           verificar_pos(matriz,i,j,palavra,0,n,m);
+
+       } 
+}
 
 void ler_matriz( int matriz[][MAX], int n, int m){
     for ( int i = 0; i < n; i++)
@@ -14,14 +40,16 @@ void ler_matriz( int matriz[][MAX], int n, int m){
 
 int main(){
     int n,m,q;
-    int matriz[MAX][MAX], palavras[10];
+    char matriz[MAX][MAX], palavras[10][21];
 
 
     scanf("%d %d %d", &n, &m, &q);
     ler_matriz(matriz,n,m);
     for( int i = 0; i < q; i++){
-        scanf("%d", &palavras[i]);
+        scanf("%s", &palavras[i]);
+        comparar_nome(matriz,palavras[i],n,m);
     }
+    
 
 
 
