168021a115898c95df2144a9a84ddff77bae055b
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-10-13 20:14:48 -0300

    terminei as implementações

--- tarefa04/seaofwords.c
+++ tarefa04/seaofwords.c
@@ -5,6 +5,17 @@ n = O numero de linhas
 m = o numero de colunas do texto
 q = numero de palavras a serem buscadas
 */
+int valido(char matriz[][100], int i, int j, int n, int m){
+    // Função para checar as bordas
+    if ((i > n-1) || ( i < 0) || (j > m-1) || (j < 0) ){
+        return 0;
+    } else
+    {
+        return 1;
+    }
+    
+}
+
 int verificar_pos(char matriz[][MAX], int i, int j, char palavra[], int k,int n, int m){
     if (!valido(matriz,i,j,n,m)){
         return 0;
@@ -15,11 +26,17 @@ int verificar_pos(char matriz[][MAX], int i, int j, char palavra[], int k,int n,
     if (palavra[k] != matriz[i][j]){
         return 0;
     }
+    char aux = matriz[i][j];
     matriz[i][j] = '#';
-    verificar_pos(matriz, i+1, j, palavra, k+1,n, m);
-    verificar_pos(matriz, i-1, j, palavra, k+1,n, m);
-    verificar_pos(matriz, i, j+1, palavra, k+1,n, m);
-    verificar_pos(matriz, i, j-1, palavra, k+1,n, m);
+    
+    int a = verificar_pos(matriz, i+1, j, palavra, k+1,n, m);
+    int b = verificar_pos(matriz, i-1, j, palavra, k+1,n, m);
+    int c = verificar_pos(matriz, i, j+1, palavra, k+1,n, m);
+    int d = verificar_pos(matriz, i, j-1, palavra, k+1,n, m);
+    matriz[i][j] = aux;
+    if (a||b||c||d){
+        return 1;
+    }
 }
 
 
@@ -27,7 +44,11 @@ void comparar_nome(char matriz[][MAX], char palavra[], int n, int m){
     for ( int i = 0; i < n; i++)
         for ( int j = 0; j < m; j++)
        if ( matriz[i][j] == palavra[0]){
-           verificar_pos(matriz,i,j,palavra,0,n,m);
+           int id = verificar_pos(matriz,i,j,palavra,1,n,m);
+           if ( id == 1)
+           printf("sim\n");
+           else
+           printf("não\n");
 
        } 
 }
