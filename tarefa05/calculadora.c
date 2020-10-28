3f69c0c7ec231408fc44414cfdeaa49b5f46e81a
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-10-27 23:12:40 -0300

    Função ler entrada

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -16,6 +16,41 @@ typedef struct Natural{
 
 } Natural;
 
+void inserir_dir( Natural *x, int digito){
+    no* novo_no = malloc(sizeof(no));
+    novo_no->valor = digito;
+    novo_no->dir = NULL;
+    
+    if (x->mais_dir == NULL){
+        novo_no->esq = NULL;
+        x->mais_dir = novo_no;
+        x->mais_esq = novo_no;    
+    } else{
+        novo_no->esq = x->mais_dir;
+        x->mais_dir->dir = novo_no; 
+        x->mais_dir = novo_no;
+        
+    }
+}
+
+
+Natural ler_natural(){
+    char digito;
+    Natural rv = {NULL,NULL};
+    digito = getchar();
+    while( digito < '0' || digito > '9')
+    {
+        digito = getchar();
+    }
+    while (digito >= '0' && digito <= '9')
+    {
+        inserir_dir(&rv, digito - '0');
+        digito = getchar();
+    }
+    return rv;
+
+    
+}
 
 
 
