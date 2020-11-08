1423386e028f546e1c589ac89514ab0e27823e02
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-11-07 22:02:20 -0300

    alterei meus deques e main

--- tarefa06/sala_de_espera.c
+++ tarefa06/sala_de_espera.c
@@ -2,25 +2,10 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef struct Paciente Paciente;
+typedef struct Node Node;
+typedef struct Lista_atendimento Lista_atendimento;
 
-typedef struct Lista_atendimento{
-int Valor;
-Lista_atendimento *prox;
-}Lista_atendimento;
-
-
-typedef struct Especialidade{
-    int qtd_profissionais;
-    Deques lista_de_espera;
-}Especialidade;
-
-
-typedef struct Paciente{
-    char Nome[51];
-    char prioridade;
-    Lista_atendimento atendimento;
-    int chegada;
-}Paciente;
 
 typedef struct Node
 {
@@ -29,11 +14,41 @@ typedef struct Node
     Node *ant;
 }Node;
 
-
 typedef struct Deques{
- Node*ini;
- Node*fim;
+    Node*ini;
+    Node*fim;
 }Deques;
+
+typedef struct Lista_atendimento{
+    no_Lista_atendimento *ini;
+    no_Lista_atendimento *fim;
+} Lista_atendimento;
+
+typedef struct no_Lista_atendimento{
+int Valor;
+Lista_atendimento *prox;
+}no_Lista_atendimento;
+
+typedef struct Paciente{
+    char Nome[51];
+    char prioridade;
+    Lista_atendimento atendimento;
+    int chegada;
+    int qtd_atendimentos = 0;
+}Paciente;
+
+typedef struct Especialidade{
+    int qtd_profissionais;
+    Deques lista_de_espera;
+}Especialidade;
+
+
+
+
+
+
+
+
 
 
 void inicializar_deque(Deques *deque){
@@ -43,20 +58,41 @@ void inicializar_deque(Deques *deque){
 void inserir_inicio_deque(Deques *deque, Paciente*pont_paciente){
     Node *novo_no = malloc(sizeof(Node));
     novo_no->paciente = pont_paciente;
-    novo_no->prox = deque->ini;
-    novo_no->ant = deque->ini->ant;
-    deque->ini->ant = novo_no;
-    deque->ini = novo_no;    
+    
+    if (deque->ini){
+        novo_no->prox = deque->ini;
+        novo_no->ant = deque->ini->ant;
+        deque->ini->ant = novo_no;
+        deque->ini = novo_no;
+    } else{
+        deque->ini = novo_no;
+        deque->fim = novo_no;   
+    }
+    
+        
 
 }
 
 void inserir_final_deque(Deques *deque, Paciente *pont_paciente){
     Node *novo_no = malloc(sizeof(Node));
     novo_no->paciente = pont_paciente;
-    novo_no->ant = deque->fim;
-    deque->fim->prox = novo_no;
-    deque->fim = novo_no;
-    novo_no->prox = NULL;
+    if (deque->fim){
+        novo_no->ant = deque->fim;
+        deque->fim->prox = novo_no;
+        deque->fim = novo_no;
+        novo_no->prox = NULL;
+    } else{
+        deque->ini = novo_no;
+        deque->fim = novo_no;
+    } 
+}
+
+void retirar_inicio_deque(Deques *deque, Paciente *p){
+    Node *aux = malloc(sizeof(Node));
+    aux = deque->ini;
+    deque->ini = aux->prox;
+    free(aux);
+    
 }
 
 void inicializar(Especialidade cecom[]){
@@ -69,6 +105,23 @@ void inicializar(Especialidade cecom[]){
 
 }
 
+void adicionar_atendimento_pac(int id, Paciente* paciente){
+    no_Lista_atendimento *novo_no = malloc(sizeof(no_Lista_atendimento));
+    novo_no->Valor = id;
+    novo_no->prox = NULL;
+    if ( paciente->atendimento.ini){
+        paciente->atendimento.fim->prox = novo_no;
+        paciente->atendimento.fim = novo_no;
+    } else{
+        
+        paciente->atendimento.ini = novo_no;
+        paciente->atendimento.fim = novo_no;
+    }
+}
+
+
+
+
 
 int main(){
     char nome[51], prioridade;
@@ -76,28 +129,30 @@ int main(){
     int id;
     inicializar(cecom);
     Deques lista_paciente;
+    Deques Lista_atendidos;
     lista_paciente.fim = NULL;
     lista_paciente.ini = NULL;
-
-    
-
-    
-
-
-    
+    int ordem_chegada = 0;
 
 
 
-    while(scanf("\"%[^\"]\" %c%*s", &nome, &prioridade) != EOF){
+    while(scanf("\"%[^\"]\" %c%*s", nome, &prioridade) != EOF){
+        
         Paciente *paciente = (Paciente*)malloc(sizeof(Paciente));
+        ordem_chegada++;
         strcpy(paciente->Nome,nome);
         paciente->prioridade = prioridade;
+        paciente->chegada = ordem_chegada;
+        int contador_atd = 0;
+        
         while(scanf("%d",&id) > 0){
             adicionar_atendimento_pac(id,paciente);
+            paciente->qtd_atendimentos += 1;
         }
-        adicionar_paciente(paciente,&lista_paciente);
+        inserir_final_deque(paciente,&lista_paciente);
     }
-    while()
+    atender_pacientes();
+    //while()
     
-    adicionar_cecom(&cecom[0].lista_de_espera,paciente); 
+    //adicionar_cecom(&cecom[0].lista_de_espera,paciente); 
 }
