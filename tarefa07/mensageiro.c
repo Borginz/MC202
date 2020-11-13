81e0f4b46d8cad60ad12fbf9ee8e8a0a28f37684
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-11-13 16:18:28 -0300

    Funções arvores

--- tarefa07/mensageiro.c
+++ tarefa07/mensageiro.c
@@ -3,12 +3,23 @@
 #include<string.h>
 
 typedef struct No_arvore{
-    No_arvore *dir;
-    No_arvore *esq;
+    struct No_arvore *dir;
+    struct No_arvore *esq;
     long int valor;
     char palavra[6];
-
 }No_arvore;
+
+
+
+typedef struct Triade{
+    No_arvore *x;
+    No_arvore *y;
+    No_arvore *z;
+}Triade;
+
+
+
+
 
 No_arvore* inserir_arvore( No_arvore* raiz, No_arvore *novo){
     if (raiz == NULL){
@@ -19,17 +30,59 @@ No_arvore* inserir_arvore( No_arvore* raiz, No_arvore *novo){
         raiz->dir = inserir_arvore(raiz->dir, novo);
     }
     return raiz;
-    }
-   
+}
 
+No_arvore* minimo(No_arvore* raiz){
+    if ( raiz == NULL || raiz->esq == NULL){
+        return raiz;
+    }
+    return minimo(raiz->esq);
+}
+
+
+
+
+void buscar_triade_x(No_arvore* raiz_ori, No_arvore* raiz_atual, int id, Triade* triade ){
+    if (raiz_atual != NULL){
+        buscar_triade_x(raiz_ori,raiz_atual->esq,id,triade);
+        if(triade == NULL){
+            return;
+        }
+    }
+    triade->x = raiz_atual;
+    buscar_triade_y(raiz_ori,raiz_ori,(id-triade->x->valor),triade);
+    if ( triade != NULL){
+        return;
+    }
+    if ( raiz_atual->dir != NULL){
+        buscar_triade_x(raiz_ori,raiz_atual->dir,id,triade);
+    }
+}
+
+void buscar_triade_y(No_arvore *raiz_ori,No_arvore *raiz_atual,int id,Triade *triade){
+    if (raiz_atual->esq != NULL && raiz_atual->valor > triade->x->valor){
+        buscar_triade_y(raiz_ori,raiz_atual->esq,id,triade);
+        if ( triade != NULL){
+            return;
+        }
+    }
+    triade->y = raiz_atual;
+    triade->z = buscar_triade_z(raiz_ori,(id-triade->y->valor));
+    
+
+}
 
 
 
 int main(){
     int m,n,valor;
-    long int id;
+    long int id = 0;
     No_arvore *raiz = NULL;
     char palavra[6];
+    Triade triade;
+    triade.x = NULL;
+    triade.y = NULL;
+    triade.z = NULL;
 
     while (scanf("%d %d", &m, &n) != EOF){
         for ( int i = 0; i < m; i++){
@@ -37,13 +90,17 @@ int main(){
             No_arvore *novo_no = malloc(sizeof(No_arvore));
             strcpy(novo_no->palavra,palavra);
             novo_no->valor = valor;
-            novo_no->dir,novo_no->esq = NULL;
+            novo_no->dir = NULL;
+            novo_no->esq = NULL;
             raiz = inserir_arvore(raiz,novo_no);
         }
-        scanf("%d", id);
-        buscar_triade(raiz,id);
+        for ( int i = 0; i < n;i++){
+            scanf("%ld",&id);
+            buscar_triade_x(raiz,raiz,id,&triade);
+
+        }
+        //imprimir_arvore(raiz);
+        //liberar_arvore(raiz);
         
-
-
     }
 }
