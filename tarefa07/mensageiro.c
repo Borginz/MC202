ece9ca01d178104415a8279b210b4fd76671c7e9
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-11-15 23:37:27 -0300

    Terminei o lab

--- tarefa07/mensageiro.c
+++ tarefa07/mensageiro.c
@@ -17,6 +17,8 @@ typedef struct Triade
     No_arvore *z;
 } Triade;
 
+
+// Função para inserir na arvore recursivamente analisando a arvore binaria
 No_arvore *inserir_arvore(No_arvore *raiz, No_arvore *novo)
 {
     if (raiz == NULL)
@@ -39,7 +41,6 @@ No_arvore *buscar_triade_z(No_arvore *raiz, long int k)
         return NULL;
     if (raiz == NULL || raiz->valor == k)
         return raiz;
-    //fprintf(stderr,"z atual :%ld\n", raiz->valor);
     if (raiz->valor > k)
         return buscar_triade_z(raiz->esq, k);
     else
@@ -62,11 +63,9 @@ void buscar_triade_y(No_arvore *raiz_ori, No_arvore *raiz_atual, long int id, Tr
     }
 
     triade->y = raiz_atual;
-    //fprintf(stderr,"to olhando y = %ld\n", raiz_atual->valor);
     triade->z = buscar_triade_z(raiz_ori, (id - triade->y->valor));
     if (triade->z != NULL)
     {
-        //fprintf(stderr,"achei o z que completa : %ld\n", triade->z->valor);
         return;
     }
     if (raiz_atual->dir != NULL /*&& raiz_atual->valor < id*/)
@@ -79,7 +78,7 @@ void buscar_triade_y(No_arvore *raiz_ori, No_arvore *raiz_atual, long int id, Tr
     }
     triade->y = NULL;
 }
-
+// Função para buscar x recursivamente 
 void buscar_triade_x(No_arvore *raiz_ori, No_arvore *raiz_atual, long int id, Triade *triade)
 {
     if (triade->y != NULL)
@@ -95,11 +94,9 @@ void buscar_triade_x(No_arvore *raiz_ori, No_arvore *raiz_atual, long int id, Tr
         }
     }
     triade->x = raiz_atual;
-    //fprintf(stderr,"XXXXXXto olhando x = %ld\n", raiz_atual->valor);
     buscar_triade_y(raiz_ori, raiz_ori, (id - triade->x->valor), triade);
     if (triade->y != NULL)
     {
-        //fprintf(stderr,"SAI\n");
         return;
     }
     if (raiz_atual->dir != NULL)
@@ -108,11 +105,12 @@ void buscar_triade_x(No_arvore *raiz_ori, No_arvore *raiz_atual, long int id, Tr
     }
 }
 
-void remover_sucessor(No_arvore *raiz)
+No_arvore *remover_sucessor(No_arvore *raiz, Triade *triade)
 {
 
-    if (raiz->dir == NULL){
-        return;
+    if (raiz->dir == NULL)
+    {
+        return NULL;
     }
     No_arvore *min = raiz->dir;
     No_arvore *pai = raiz;
@@ -128,35 +126,51 @@ void remover_sucessor(No_arvore *raiz)
 
     raiz->valor = min->valor;
     strcpy(raiz->palavra, min->palavra);
-    //free(min);
+    if ( triade->x == min){
+        triade->x = raiz;
+    }
+    if ( triade->y == min){
+        triade->y = raiz;
+    }
+    if ( triade->z == min){
+        triade->z = raiz;
+    }
+    free(min);
+    return raiz;
 }
 
-No_arvore *remover_triade(No_arvore *raiz, long int chave)
+No_arvore *remover_triade(No_arvore *raiz, long int chave, Triade* triade)
 {
     No_arvore *aux;
-
     if (raiz == NULL)
+    {
         return NULL;
+    }
+    if (chave == raiz->valor)
+    {
+        if (raiz->esq == NULL)
+        {
+            aux = raiz->dir;
+            free(raiz);
+            return aux;
+        }
+        else if (raiz->dir == NULL)
+        {
+            aux = raiz->esq;
+            free(raiz);
+            return aux;
+        }
+        else if (raiz->dir == NULL && raiz->esq == NULL)
+        {
+            free(raiz);
+            return NULL;
+        }
+        return remover_sucessor(raiz, triade);
+    }
     if (chave < raiz->valor)
-        raiz->esq = remover_triade(raiz->esq, chave);
+        raiz->esq = remover_triade(raiz->esq, chave, triade);
     else if (chave > raiz->valor)
-        raiz->dir = remover_triade(raiz->dir, chave);
-
-    else if (raiz->esq == NULL)
-    {
-        aux = raiz->dir;
-        free(raiz);
-        return aux;
-    }
-    else if (raiz->dir == NULL)
-    {
-        aux = raiz->esq;
-        free(raiz);
-        return aux;
-    }
-
-    else
-        remover_sucessor(raiz);
+        raiz->dir = remover_triade(raiz->dir, chave, triade);
     return raiz;
 }
 
@@ -218,34 +232,23 @@ int main()
         for (int i = 0; i < n; i++)
         {
             scanf("%ld", &id);
-            //fprintf(stderr,"TO PROCURANDO ID:%ld\n",id);
             buscar_triade_x(raiz, raiz, id, triade);
-            //fprintf(stderr,"%d %d %d\n", triade->x == NULL,triade->y == NULL,triade->z == NULL);
-            //fprintf(stderr,"%ld %ld %ld\n", triade->x->valor, triade->y->valor,triade->z->valor);
-            //fprintf(stderr, "%s %s %s\n", triade->x->palavra, triade->y->palavra,triade->z->palavra);
             strcpy(palavra_copia, triade->x->palavra);
             valor_x = triade->x->valor;
-            //fprintf(stderr,"Print antes\n");
-            raiz = remover_triade(raiz, triade->x->valor);
             strcat(palavra_copia, triade->y->palavra);
             valor_y = triade->y->valor;
-            raiz = remover_triade(raiz, triade->y->valor);
             strcat(palavra_copia, triade->z->palavra);
-            //fprintf(stderr,"%s\n", palavra_copia);
             valor_z = triade->z->valor;
-            raiz = remover_triade(raiz, triade->z->valor);
-            //fprintf(stderr,"removi todos\n");
             valor_id = valor_x + valor_y + valor_z;
-            No_arvore *no_adc = calloc(sizeof(No_arvore), 1);
+            No_arvore *no_adc = malloc(sizeof(No_arvore));
             no_adc->dir = NULL;
             no_adc->esq = NULL;
             strcpy(no_adc->palavra, palavra_copia);
             no_adc->valor = valor_id;
-            //fprintf(stderr,"valor do no: %ld\n", no_adc->valor);
-            //fprintf(stderr,"palavra:%s\n", no_adc->palavra);
             raiz = inserir_arvore(raiz, no_adc);
-            //fprintf(stderr,"terminei de inserir\n");
-            //fprintf(stderr,"Chamei\n");
+            raiz = remover_triade(raiz, triade->x->valor,triade);
+            raiz = remover_triade(raiz, triade->y->valor,triade);
+            raiz = remover_triade(raiz, triade->z->valor,triade);
             strcpy(palavra_copia, "");
             triade->x = NULL;
             triade->y = NULL;
@@ -256,4 +259,5 @@ int main()
         liberar_arvore(raiz);
         raiz = NULL;
     }
+    free(triade);
 }
