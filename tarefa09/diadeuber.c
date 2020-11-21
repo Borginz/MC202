b9c859799ef2499cae8ce0377df002f5a70b0744
Leonardo de Queiroz Borges <l177829@dac.unicamp.br>
2020-11-20 21:53:02 -0300

    terminei a main e a função remover

--- tarefa09/diadeuber.c
+++ tarefa09/diadeuber.c
@@ -19,15 +19,12 @@ typedef struct Fila_prioridade
     int n, tamanho;
 } FP;
 
-
 void troca(cliente *a, cliente *b)
 {
     cliente t = *a;
     *a = *b;
     *b = t;
 }
-
-
 
 FP *criar_filaprio(int tam)
 {
@@ -80,40 +77,90 @@ cliente extrai_maximo(FP *fprio)
     return item;
 }
 
- FP* remover_procurado(FP * fprio,char procurado[]);
+FP *remover_procurado(FP *fprio, char procurado[])
+{
+    for (int i = 0; i < fprio->n; i++)
+    {
+        if (strcmp(fprio->v[i].Nome, procurado) == 0)
+        {
+            fprio->v[i].avaliacao = 6;
+            sobe_no_heap(fprio,6);
+            extrai_maximo(fprio);
+        }
+    }
+    return fprio;
+}
 
 int main()
 {
-    int distancia_total = 0, distancia_cliente = 0, bruto = 0, despesas = 0, liquido = 0;
+    int distancia_total = 0, distancia_cliente = 0, pos_mot_x = 0,pos_mot_y = 0;
     char x;
-    FP* fprio;
+    double liquido = 0, bruto = 0, despesas = 0;
+    FP *fprio;
     fprio = criar_filaprio(250);
     cliente cliente_heap;
     cliente cliente_atual;
-    char procurado[16];
+    strcpy(cliente_atual.Nome, "Leonardo");
+    cliente_atual.avaliacao = 0;
+    cliente_atual.pos_fim_x = 0;
+    cliente_atual.pos_fim_y = 0;
+    cliente_atual.pos_ini_x = 0;
+    cliente_atual.pos_ini_y = 0;
+    char procurado[20];
 
-    while (scanf("%c ", &x) != EOF){
-        switch(x){
-            case 'A':
-                scanf("%s %lf %d %d %d %d", cliente_heap.Nome,&cliente_heap.avaliacao,&cliente_heap.pos_ini_x,&cliente_heap.pos_ini_y,&cliente_heap.pos_fim_x,&cliente_heap.pos_fim_y );
-                if (cliente_atual.avaliacao != 0){
-                    insere(fprio,cliente_heap);
-                } else{
-                    cliente_atual = cliente_heap;
-                }
-                printf("Cliente %s foi adicionado(a)\n", cliente_heap.Nome);
-            case 'C':
-                scanf("%s", procurado);
-                fprio = remover_procurado(fprio,procurado);
-                bruto += 7;
-                printf("%s cancelou a corrida\n",procurado);
-            case 'F':
-                if (fprio->n > 0){
-                    cliente_atual = extrai_maximo(fprio);
-                } else{
+    while (scanf("%c ", &x) != EOF)
+    {
+        switch (x)
+        {
+        case 'A':
+            scanf("%s %lf %d %d %d %d", cliente_heap.Nome, &(cliente_heap.avaliacao), &(cliente_heap.pos_ini_x), &(cliente_heap.pos_ini_y), &(cliente_heap.pos_fim_x), &(cliente_heap.pos_fim_y));
+            if (cliente_atual.avaliacao != 0)
+            {
+                insere(fprio, cliente_heap);
+            }
+            else
+            {
+                cliente_atual = cliente_heap;
+            }
+            printf("Cliente %s foi adicionado(a)\n", cliente_heap.Nome);
+            break;
+        case 'C':
+            scanf("%s", procurado);
+            fprio = remover_procurado(fprio, procurado);
+            bruto += 7;
+            printf("%s cancelou a corrida\n", procurado);
+            break;
+        case 'F':
+            printf("A corrida de %s foi finalizada\n", cliente_atual.Nome);
+            distancia_total += abs(pos_mot_x - cliente_atual.pos_ini_x) + abs(pos_mot_y - cliente_atual.pos_ini_y);
+            distancia_cliente += abs(cliente_atual.pos_ini_x - cliente_atual.pos_fim_x) + abs(cliente_atual.pos_ini_y - cliente_atual.pos_fim_y);
+            distancia_total += distancia_cliente;
+            pos_mot_x = cliente_atual.pos_fim_x;
+            pos_mot_y = cliente_atual.pos_fim_y;
+            
+            if (fprio->n > 0)
+            {
+                cliente_atual = extrai_maximo(fprio);
+            }
+            else
+            {
+                cliente_atual.avaliacao = 0;
+            }
+            break;
 
-                }
 
+        case 'T':
+            despesas += (((distancia_total/10)*4.104) + 57);
+            bruto += distancia_cliente*1.4;
+            liquido += bruto - bruto*0.25 + despesas;
+            
+            printf("\n");
+            printf("Jornada finalizada. Aqui esta o seu rendimento de hoje\n");
+            printf("Km total: %d\n", distancia_total);
+            printf("Rendimento bruto: %.2lf\n", bruto);
+            printf("Despesas: %.2lf\n", despesas);
+            printf("Rendimento liquido: %.2lf\n", liquido);
+            break;
         }
     }
 }
